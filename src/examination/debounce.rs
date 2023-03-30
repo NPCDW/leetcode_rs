0004dc8612ac2ac16b3ef05c65f1d98cd71b9440
NPCDW <chenyongli0520@gmail.com>
2023-03-30 11:19:07 +0800

    debounce

--- src/examination/debounce.rs
+++ src/examination/debounce.rs
@@ -27,6 +27,43 @@ fn debounce<T: Send + Debug + 'static>(
     tx
 }
 
+#[allow(dead_code)]
+pub fn debounce_forever<T: Send + Debug + 'static>(
+    f: impl Fn(T) + Send + 'static,
+    debounce_time: Duration,
+) -> Sender<T> {
+    let (tx, rx) = channel::<T>();
+    thread::spawn(move || {
+        loop {
+            match rx.recv() {
+                Ok(value) => {
+                    let mut last_value = Some(value);
+                    println!("recv {:?}", &last_value);
+                    loop {
+                        match rx.recv_timeout(debounce_time) {
+                            Ok(value) => {
+                                last_value = Some(value);
+                                println!("recv timeout {:?}", &last_value);
+                            }
+                            Err(_) => {
+                                println!("recv timeout finally {:?}", &last_value);
+                                if last_value.is_some() {
+                                    f(last_value.unwrap());
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+                Err(_) => {
+                    break;
+                }
+            }
+        }
+    });
+    tx
+}
+
 #[cfg(test)]
 mod debounce_test {
     use std::time::Duration;
@@ -49,6 +86,26 @@ mod debounce_test {
             }, Duration::from_millis(500));
             tx.send(4).unwrap();
         }
+        tx.send(5).unwrap();
+        thread::sleep(Duration::from_millis(400));
+        tx.send(6).unwrap();
+        thread::sleep(Duration::from_millis(400));
+        tx.send(7).unwrap();
+        thread::sleep(Duration::from_millis(400));
+        tx.send(8).unwrap();
+        thread::sleep(Duration::from_millis(2000));
+    }
+    
+    #[test]
+    fn debounce_forever_test() {
+        let tx = debounce(|x| {
+            println!("debounce: {:?}", x);
+        }, Duration::from_millis(500));
+        tx.send(1).unwrap();
+        tx.send(2).unwrap();
+        tx.send(3).unwrap();
+        thread::sleep(Duration::from_millis(600));
+        tx.send(4).unwrap();
         tx.send(5).unwrap();
         thread::sleep(Duration::from_millis(400));
         tx.send(6).unwrap();
